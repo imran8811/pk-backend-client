2f7f725bb2c828f33525d283de7843c7db62cdc4
Mohd Imran <Mohd Imran>
2021-05-09 20:30:21 +0500

    changes

--- app.ts
+++ app.ts
@@ -18,13 +18,20 @@ var app = express();
 app.use(cors());
 
 //CORS middleware
-var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-}
+// var allowCrossDomain = function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// }
 
-app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 var indexRouter = require('./routes/index.routes.ts');
 var usersRouter = require('./routes/users.routes.ts');
